cc16408ecfeaeb1ab973af8044dbe8f307666d7a
ej <ej8899@gmail.com>
2023-11-07 16:39:46 -0500

    update: data types

---
+++ 02-basics/05-datatypes.py
@@ -0,0 +1,15 @@
+# strings
+# int
+# float 1.234
+# boolean
+
+# list [1,2,'test']
+# lists can be nested
+
+# tuple (1,2,'test')
+# tuple can't be changed!
+
+# set {1,2,'test'}
+#dictionary (js object)
+a={'a':1,'b':5,'c':'hello'}
+print (a['c'])
