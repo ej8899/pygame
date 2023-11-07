0cf477774250dcb06a019866144867418584e70c
ej <ej8899@gmail.com>
2023-11-07 16:24:31 -0500

    update: comment types in python

--- 02-basics/03-return.py
+++ 02-basics/03-return.py
@@ -1,3 +1,19 @@
 test_var = 'A word'.upper().replace('A','X')
 
-print(test_var)
+print(test_var) # comments here ok
+
+
+# single line comments
+
+
+# another_var = 564.54
+# another_var = 564.54
+# another_var = 564.54
+# another_var = 564.54
+# print(another_var)
+
+'''
+this is how we can do multi line comments.
+print("hi there")
+above won't work despite not being greyed out.
+'''
