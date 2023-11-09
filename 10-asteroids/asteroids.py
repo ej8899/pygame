beaac1105a11c1d074da084a564828ec6c5bfbd3
ej8899 <ej8899@gmail.com>
2023-11-09 10:46:55 -0500

    update: center ship

--- 10-asteroids/asteroids.py
+++ 10-asteroids/asteroids.py
@@ -6,10 +6,17 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 GAME_TITLE = "Asteroid Shooter"
 
+
+# important references:
+# color list: https://www.pygame.org/docs/ref/color_list.html
+
+
+
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 display_surface.fill(pygame.Color('grey46'))
 # display_surface.fill((255,0,255)) # as RGB
 
+clock = pygame.time.Clock(); # use for frame rate limiter
 
 pygame.display.set_caption(GAME_TITLE)
 
@@ -23,6 +30,10 @@ def writeCentered(text, x, y, color="Coral",):
 
 # image imports
 ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha(); # for alpha transparencies - or just .convert() if not.
+ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+ship_x_pos = 300;
+ship_y_pos = 500;
+
 background_surf = pygame.image.load('./graphics/background.png').convert();
 
 # font imports
@@ -40,14 +51,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit();
             sys.exit();
+    
+    # framerate limit
+    clock.tick(120);
 
     # update game parts
 
     # place graphics
     display_surface.fill(pygame.Color('grey46'))
     display_surface.blit(background_surf,(0,0))
-    display_surface.blit(ship_surf,(300,500))
+    display_surface.blit(ship_surf,ship_rect)
     display_surface.blit(text_surf,text_rect)
 
     # show display surface
     pygame.display.update()
+# end game loop
