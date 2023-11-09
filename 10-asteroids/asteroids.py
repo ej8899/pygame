8e20c502adfad6eb8f08e8b92b86fa91561f8489
ej8899 <ej8899@gmail.com>
2023-11-08 21:30:23 -0500

    update: working w surfaces in pygame

--- 10-asteroids/asteroids.py
+++ 10-asteroids/asteroids.py
@@ -4,8 +4,15 @@ pygame.init()
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+GAME_TITLE = "Asteroid Shooter"
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface.fill(pygame.Color('azure1'))
+
+pygame.display.set_caption(GAME_TITLE)
+
+test_surface = pygame.Surface((400,100)) # x & y coords
+test_surface.fill(pygame.Color('lightskyblue4'))
 
 # start our game loop
 while True:
@@ -17,9 +24,8 @@ while True:
 
     # update game parts
 
-
     # place graphics
-    
+    display_surface.blit(test_surface,(20,50))
 
     # show display surface
     pygame.display.update()
