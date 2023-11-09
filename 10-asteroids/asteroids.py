389462547ccc2038d0d6ce7f2dd31661435ba569
ej8899 <ej8899@gmail.com>
2023-11-09 10:19:08 -0500

    add: ship graphics, fonts, background

--- 10-asteroids/asteroids.py
+++ 10-asteroids/asteroids.py
@@ -10,10 +10,28 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 display_surface.fill(pygame.Color('grey46'))
 # display_surface.fill((255,0,255)) # as RGB
 
+
 pygame.display.set_caption(GAME_TITLE)
 
-test_surface = pygame.Surface((400,100)) # x & y coords
-test_surface.fill(pygame.Color('lightskyblue4'))
+# helper functions
+def writeCentered(text, x, y, color="Coral",):
+    text = font.render(text, True, pygame.Color(color))
+    text_rect = text.get_rect(center=(WINDOW_WIDTH//2, y))
+    return text, text_rect
+
+
+
+# image imports
+ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha(); # for alpha transparencies - or just .convert() if not.
+background_surf = pygame.image.load('./graphics/background.png').convert();
+
+# font imports
+font = pygame.font.Font('./graphics/subatomic.ttf', 50);
+# text_surf = font.render(GAME_TITLE, True, pygame.Color('darkorange')); # text, antialiasing (true or false), color
+# text_rect = text_surf.get_rect(center=(WINDOW_WIDTH/2,680))
+
+
+text_surf, text_rect = writeCentered(GAME_TITLE, 0, 680,'darkorange') # this will be centered anyhow, but at 10 height
 
 # start our game loop
 while True:
@@ -26,7 +44,10 @@ while True:
     # update game parts
 
     # place graphics
-    display_surface.blit(test_surface,(WINDOW_WIDTH-400,150))
+    display_surface.fill(pygame.Color('grey46'))
+    display_surface.blit(background_surf,(0,0))
+    display_surface.blit(ship_surf,(300,500))
+    display_surface.blit(text_surf,text_rect)
 
     # show display surface
     pygame.display.update()
